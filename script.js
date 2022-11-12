3d02a700d147a03f297e80915c0223a2449155d4
Jakub <rboweel@gmail.com>
2022-11-12 11:27:03 +0100

    added header

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+setInterval(spotify, 3000);
+setInterval(nordvpn, 4000);
+setInterval(netflix, 5000);
+setInterval(disney, 6000);
+
+function spotify() {
+    let changer = document.getElementById('changer').innerHTML = 'spotify ';
+}
+function nordvpn() {
+    let changer = document.getElementById('changer').innerHTML = 'nordvpn ';
+}
+function netflix() {
+    let changer = document.getElementById('changer').innerHTML = 'netflix ';
+}
+function disney() {
+    let changer = document.getElementById('changer').innerHTML = 'disney+ ';
+}
